78f729380fb1a31e780884ccde7269ae2ad13f32
Sargis <saqo1995s@gmail.com>
2025-02-19 19:03:21 -0800

    fixed NavigationBar logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { Breadcrumb, Layout, theme } from 'antd';
 import NavigationBar from './components/NavigationBar';
 
-import Styles from './styles/App.css'
+// Pages
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+
+// import Styles from './styles/App.css'
 import SideBar from './components/SideBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -13,10 +19,11 @@ const App: React.FC = () => {
   } = theme.useToken();
 
   return (
+    <Router>
     <Layout style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
       <Header>
         <div className="demo-logo" />
-        <NavigationBar />
+          <NavigationBar />
       </Header>
       <div style={{ padding: '0 2%' }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
@@ -28,13 +35,20 @@ const App: React.FC = () => {
           <Sider >
             <SideBar />
           </Sider>
-          <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contacts" element={<ContactPage />} />
+          </Routes>
+          </Content>
         </Layout>
       </div>
       <Footer style={{ textAlign: 'center', paddingTop: '100px' }}>
         Sargis Tovmasyan ©{new Date().getFullYear()}
       </Footer>
     </Layout>
+    </Router>
   );
 };
 
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,20 +1,29 @@
 import { Menu, type MenuProps } from 'antd';
-
-const options: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
-    key,
-    label: `nav ${key}`,
-  }));
+import { useNavigate } from 'react-router-dom';
 
 const NavigationBar: React.FC = () => {
-    return (
-        <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        items={options}
-        style={{ flex: 1, minWidth: 0 }}
-      /> 
-    );
+  const navigate = useNavigate();
+
+  const options: MenuProps['items'] = [
+    { key: '/', label: 'Home' },
+    { key: '/about', label: 'About' },
+    { key: '/contacts', label: 'Contact' },
+  ];
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    navigate(e.key);
+  };
+
+  return (
+    <Menu
+      theme="dark"
+      mode="horizontal"
+      items={options}
+      onClick={onClick}
+      style={{ flex: 1, minWidth: 0 }}
+    />
+  );
 };
 
 export default NavigationBar;
+
